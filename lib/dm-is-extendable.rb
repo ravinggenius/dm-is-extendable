9213c9f1773a8cc23d8d6bdd9542e10e98dd9d81
Raving Genius <rg+code@ravinggenius.com>
2009-11-14 10:52:59 -0500

    Small change

--- lib/dm-is-extendable.rb
+++ lib/dm-is-extendable.rb
@@ -1,5 +1,4 @@
 # Needed to import datamapper and other gems
-require 'rubygems'
 require 'pathname'
 
 # Add all external dependencies for the plugin here
@@ -8,11 +7,14 @@ require 'dm-core'
 
 # Require plugin-files
 require Pathname(__FILE__).dirname.expand_path / 'dm-is-extendable' / 'is' / 'extendable.rb'
-
+require Pathname(__FILE__).dirname.expand_path / 'dm-is-extendable' / 'is' / 'extension.rb'
+require Pathname(__FILE__).dirname.expand_path / 'dm-is-extendable' / 'is' / 'version.rb'
 
 # Include the plugin in Resource
 module DataMapper
   module Model
     include DataMapper::Is::Extendable
+    include DataMapper::Is::Extension
+    include DataMapper::Is::Version
   end # module Model
 end # module DataMapper
